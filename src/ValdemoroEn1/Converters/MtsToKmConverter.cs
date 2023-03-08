221f4a1e5c054def93f123f399980762b77e162a
Ángel Rubén Valdeolmos Carmona <angel.valdeolmos@outlook.es>
2023-03-08 14:37:58 +0100

    Weather

---
+++ src/ValdemoroEn1/Converters/MtsToKmConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ValdemoroEn1.Converters;
+
+public class MtsToKmConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return null;
+
+        double mts = (double)value;
+        int km = System.Convert.ToInt32(mts * 3.6);
+        string final = string.Concat(km.ToString(), "km/h");
+        return final;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
