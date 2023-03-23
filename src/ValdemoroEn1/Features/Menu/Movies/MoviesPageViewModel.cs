2a56f460338e85fd8e971d3c67bfe0781ea656ef
Ángel Rubén Valdeolmos Carmona <angel.valdeolmos@outlook.es>
2023-03-23 17:15:23 +0100

    fie #1

--- src/ValdemoroEn1/Features/Menu/Movies/MoviesPageViewModel.cs
+++ src/ValdemoroEn1/Features/Menu/Movies/MoviesPageViewModel.cs
@@ -77,7 +77,7 @@ public partial class MoviesPageViewModel : BaseViewModel
 
     private List<string> FlattenDates()
     {
-        var dates = events.SelectMany(s => s.InfoFechas.Fechas.Select(g => DateTime.Parse(g.LaFecha))).Distinct().Take(7).Order().Select(s => s.ToString("dddd d MMMM")).ToList();
+        var dates = events.SelectMany(s => s.InfoFechas.Fechas.Select(g => DateTime.Parse(g.LaFecha))).Distinct().Take(8).Order().Select(s => s.ToString("dddd d MMMM")).ToList();
         return dates;
     }
 }
