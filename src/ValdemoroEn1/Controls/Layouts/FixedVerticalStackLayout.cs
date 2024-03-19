641f33f3e5ae813cc7a856ca1658afa219022eae
Ángel Rubén Valdeolmos Carmona <angel.valdeolmos@outlook.es>
2024-03-19 15:19:43 +0100

    updates

---
+++ src/ValdemoroEn1/Controls/Layouts/FixedVerticalStackLayout.cs
@@ -0,0 +1,20 @@
+﻿namespace ValdemoroEn1.Controls;
+
+//TODO BUG: https://github.com/dotnet/maui/issues/13577
+
+public class FixedVerticalStackLayout : VerticalStackLayout
+{
+
+    public FixedVerticalStackLayout() : base()
+    {
+
+    }
+
+    protected override void OnAdd(int index, IView view)
+    {
+        base.OnAdd(index, view);
+#if ANDROID
+        Dispatcher.Dispatch(InvalidateMeasure);
+#endif
+    }
+}
